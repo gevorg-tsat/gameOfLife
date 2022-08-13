f2cf2d9dcefd0a085503b60302c5fe476ac24cb2
Knot Hulk <knothulk@mi-s4.msk.21-school.ru>
2022-08-13 15:17:59 +0300

    seg fauls

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -4,8 +4,27 @@
 int allocate(int ***matrix, int n, int m);
 void update(int ***matrix, int n, int m);
 int check_life(int **matrix, int n, int m, int row, int column);
+void output(int **matr, int n, int m);
+
 
 int main() {
+    int **matrix = NULL;
+    allocate(&matrix, 20, 20);
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++)
+            matrix[i][j] = 0;
+    }
+    matrix[5][6] = 1;
+    matrix[5][8] = 1;
+    matrix[6][7] = 1;
+    matrix[6][8] = 1;
+    matrix[7][6] = 1;
+    output(matrix, 20, 20);
+    while(getchar() == '\n') {
+        printf("\x1B[2J\x1B[H");
+        update(&matrix, 20, 20);
+        output(matrix, 20, 20);
+    }
     return 0;
 }
 
@@ -23,12 +42,15 @@ int check_life(int **matrix, int n, int m, int row, int column) {
     int **neighbors;
     allocate(&neighbors, 8, 2);
     int counter = 0;
-    for (int i = row - 1; i <= row + 1; i++)
-        for (int j = column - 1; j <= column + 1; j++) 
+    for (int i = row - 1; i <= row + 1; i++) {
+        for (int j = column - 1; j <= column + 1; j++) {
             if (j != row && i != column) {
-                neighbors[counter++][0] = i;
-                neighbors[counter++][1] = j;
+                neighbors[counter][0] = i;
+                neighbors[counter][1] = j;
+                counter++;
             }
+        }
+    }
     for (int i = 0; i < 8; i++) {
         if (neighbors[i][0] < 0)
             neighbors[i][0] += n;
@@ -41,7 +63,7 @@ int check_life(int **matrix, int n, int m, int row, int column) {
     }
     counter = 0;
     for (int i = 0; i < 8; i++)
-        counter += matrix[neighbors[i][0]][neighbors[i][0]];
+        counter += matrix[neighbors[i][0]][neighbors[i][1]];
     if (matrix[row][column] && (counter == 2 || counter == 3)) {
         free(neighbors);
         return 1;
@@ -63,3 +85,15 @@ int allocate(int ***matrix, int n, int m) {
         (*matrix)[i] = ptr + m * i;
     return 0;
 }
+
+void output(int **matr, int n, int m) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m; j++)
+            printf("%d ", matr[i][j]);
+        printf("\n");
+    }
+    for (int j = 0; j < m - 1; j++)
+        printf("%d ", matr[n-1][j]);
+    printf("%d", matr[n-1][m-1]);
+}
+
